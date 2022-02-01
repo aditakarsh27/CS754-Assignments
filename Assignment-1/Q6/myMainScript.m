1b55ad8e3d27b6de518f3d791af9e8bedc9eaa66
aditakarsh27 <62296167+aditakarsh27@users.noreply.github.com>
2022-02-01 10:19:15 +0530

    (b)

--- Assignment-1/Q6/myMainScript.m
+++ Assignment-1/Q6/myMainScript.m
@@ -5,9 +5,21 @@ for i=1:length(cars.frames)
     cars.frames(i).cdata = rgb2gray(cars.frames(i).cdata);
 end
 T = 3;
-H = cars.height;
-W = cars.width;
-frames = zeros(H, W, T);
+H = 120;
+W = 240;
+F = zeros(H, W, T);
 for t=1:T
-    frames(:,:,t) = cars.frames(i).cdata;
+    F(:,:,t) = cars.frames(i).cdata(end-119:end,end-239:end);
 end
+
+%% (b)
+
+C = randi([0,1], H, W, T);
+E = sum(C .* F, 3);
+
+sigma = 2;
+N = normrnd(0, sigma, H, W);
+I_noisy = E + N;
+imshow(rescale(I_noisy));
+title("Coded Snapshot");
+imwrite(rescale(I_noisy), './images/coded_snap.png');
