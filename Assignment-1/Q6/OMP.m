dacf52def99a2c7b4f3636c1019b7d3bcee24334
aditakarsh27 <62296167+aditakarsh27@users.noreply.github.com>
2022-02-03 00:13:21 +0530

    Remaining parts and RMSE

--- Assignment-1/Q6/OMP.m
+++ Assignment-1/Q6/OMP.m
@@ -3,11 +3,13 @@ function theta = OMP(y, A, eps)
     theta = zeros(size(A, 2), 1);
     T = [];
     A = normc(A);
-    while (norm(r) > eps)
+    i = 0;
+    while (norm(r) > eps && i < size(A, 2))
         [~, j] = max(abs(r' * A));
         T = [T j];
         A_T = A(:, T);
         theta(T) = pinv(A_T) * y;
         r = y - A_T * theta(T);
+        i = i + 1;
     end
 end
