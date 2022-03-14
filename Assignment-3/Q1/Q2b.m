dc5c82e32baf99c8a2ff5e27f0a09395cb971048
aditakarsh27 <62296167+aditakarsh27@users.noreply.github.com>
2022-03-14 23:16:21 +0530

    Q2b

--- Assignment-3/Q1/Q2b.m
+++ Assignment-3/Q1/Q2b.m
@@ -1,20 +1,20 @@
 x = double(imread("barbara256.png"));
-n = 2*randn(size(x));
-y = x+n;
+x = padarray(x,[7, 7],0,"post");
+phi = randn(32, 64);
 U = kron(dctmtx(8)', dctmtx(8)');
-A = U;
-alpha = max(eig(A'*A))+0.1;
+A = phi*U;
+alpha = max(eig(A'*A))+5;
 counts = zeros(size(x));
 x_new = zeros(size(x));
-for i=1:1:256-7
-    for j=1:1:256-7
-        y_patch = y(i:i+7, j:j+7);
-        y_patch = y_patch(:);
-        lambda = 5/(2*alpha);
+y = zeros(size(x));
+for i=1:1:256
+    for j=1:1:256
+        y_patch = x(i:i+7, j:j+7);
+        y_patch = phi*y_patch(:);
+        lambda = 1/(2*alpha);
         theta = zeros(64, 1);
-        theta_old = zeros(64,1);
-        steps = 128;
-        error = 0.005;
+        theta_old = zeros(64, 1);
+        error = 0.05;
         theta_diff = 1 + error;
         while theta_diff > error
             theta_old = theta;
@@ -37,9 +37,11 @@ for i=1:1:256-7
     end
 end
 x_new = x_new./counts;
-imwrite(x_new,"2a_rec.png");
-imwrite(y,"2a_noisy.png");
-montage([rescale(y), rescale(x_new), rescale(x)]);
-title('Noisy Image, Reconstructed Image, Original Image')
+x_new = x_new(1:256,1:256);
+x = x(1:256,1:256);
+%%
+imwrite(rescale(x_new),"2b_rec.png");
+montage([rescale(x_new), rescale(x)]);
+title('Reconstructed Image, Original Image')
 rmse = norm(x-x_new, 'fro')/norm(x, 'fro');
 disp("rmse = " + rmse);
