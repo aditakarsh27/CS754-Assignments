b6cf448318c4d150c2107b282b2f7d8b8fe821a4
DarinJeff <darinjeff@gmail.com>
2022-05-02 16:14:04 +0530

    Generate X, Y

--- Project/reading.m
+++ Project/reading.m
@@ -1,14 +1,35 @@
-load('mnist.mat');
-%%
-X_test = double(mnist{2,1}');
-X_label = double(mnist{2,2});
-X_0 = X_test(X_label==0);
-%%
-p = 784;
-n = size(X_0,2);
-m = 400;
-s = 10;
+clc;
+% clear;
+% load('mnist1.mat');
+%% Setting up p×n matrix - X
+zero_labels = (training.labels==0);
+X = training.images(:,:,zero_labels);
+p = size(X,1)*size(X,2);
+n = size(X,3);
+X = reshape(X, [p  n]);
+
+image(reshape(X(:,36), [28 28])*255);
+%% Generate random p×m projection matrices
+m = 40;
+s = 100;
 R = binornd(1, 1/s, n,p,m);
 pos = binornd(1,0.5, n,p,m);
 pos(pos==0)= -1;
 R = R.*p;
+%% Get projectections, Y m×n and Xhat p×n
+Y = zeros(m,n);
+Xhat = zeros(p,n);
+for i=1:n
+    Ri = reshape(R(i,:,:), [p m]);
+    Y(:,i) = Ri.' * X(:,i);
+    Xhat(:,i) = Ri * Y(:,i);
+end
+%% Estimate Covariance, Σ p×p
+
+
+
+
+
+
+
+
