9a688984b58b6e22e2be7a49e6003e8bd7bacd78
aditakarsh27 <62296167+aditakarsh27@users.noreply.github.com>
2022-05-02 21:57:07 +0530

    Saving results

--- Project/myMainScript.m
+++ Project/myMainScript.m
@@ -42,12 +42,14 @@ Uk = U1(:,1:k);
 alpha = (Uk.')*img;      
 reconstructed_true = reshape((Uk * alpha) + mu, 28,28);
 figure(); imshow(reconstructed_true);
+imwrite(reconstructed_true, 'true_10.png');
 title('chosen image reconstructed from top 10 eigenvectors of sample covariance matrix');
 
 Vk = V2(:,1:k);         
 beta = (Vk.')*img;      
 reconstructed_approx = reshape((Vk * beta) + mu, 28,28);
 figure(); imshow(reconstructed_approx);
+imwrite(reconstructed_approx, 'est_10.png');
 title('chosen image reconstructed from top 10 eigenvectors of estimated covariance matrix');
 
 k = 50;
@@ -55,15 +57,18 @@ Uk = U1(:,1:k);
 alpha = (Uk.')*img;      
 reconstructed_true = reshape((Uk * alpha) + mu, 28,28);
 figure(); imshow(reconstructed_true);
+imwrite(reconstructed_true, 'true_50.png');
 title('chosen image reconstructed from top 50 eigenvectors of sample covariance matrix');
 
 Vk = V2(:,1:k);         
 beta = (Vk.')*img;      
 reconstructed_approx = reshape((Vk * beta) + mu, 28,28);
 figure(); imshow(reconstructed_approx);
+imwrite(reconstructed_approx, 'est_50.png');
 title('chosen image reconstructed from top 50 eigenvectors of estimated covariance matrix');
 
 C_n = cov(X');
+display('RMSE:');
 display(norm(C_n-sigma_hat)/norm(C_n));
 
 
